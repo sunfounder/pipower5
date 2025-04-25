9375940858141da2c9c048159687dfc4dffb064d
Lovmong <1967471283@qq.com>
2025-04-25 16:17:15 +0800

    change name

--- pipower5/__init__.py
+++ pipower5/__init__.py
@@ -1,3 +1,7 @@
+from .version import __version__
+from .pipower5 import PiPower5
+
+
 def update_config_file(config, config_path):
     import json
     from .utils import merge_dict
@@ -10,7 +14,6 @@ def update_config_file(config, config_path):
 
 def main():
     import time
-    from spc.spc import SPC
     from .pipower5 import PiPower5
     import argparse
     from .constants import PERIPHERALS
@@ -33,7 +36,7 @@ def main():
     new_sys_config = {}
     new_peripheral_config = {}
 
-    spc = SPC()
+    pipower5 = PiPower5()
     parser = argparse.ArgumentParser(prog='pipower5', description='PiPower 5')
     parser.add_argument("command",
                         choices=["start", "restart", "stop"],
@@ -106,10 +109,8 @@ def main():
 
     if args.command == "stop":
         import os
-        from .pipower5 import PiPower5
         os.system('kill -9 $(pgrep -f "pipower5 start")')
         os.system('kill -9 $(pgrep -f "pipower5-service start")')
-        pipower5 = PiPower5()
         pipower5.stop()
         quit()
 
@@ -150,81 +151,81 @@ def main():
 
     if args.shutdown_percentage != '':
         if args.shutdown_percentage == None:
-            print(f"Shutdown battery percentage: {spc.read_shutdown_percentage()}%")
+            print(f"Shutdown battery percentage: {pipower5.read_shutdown_percentage()}%")
         else:
             if int(args.shutdown_percentage) < 10:
                 print("Failed, shutdown battery percentage minimal is 10%")
             elif int(args.shutdown_percentage) > 100:
                 print("Failed, shutdown battery percentage maximal is 100%")
             else:
-                spc.write_shutdown_percentage(int(args.shutdown_percentage))
+                pipower5.write_shutdown_percentage(int(args.shutdown_percentage))
                 time.sleep(0.5)
-                if spc.read_shutdown_percentage() == int(args.shutdown_percentage):
-                    print(f"Success, shutdown battery percentage: {spc.read_shutdown_percentage()}%")
+                if pipower5.read_shutdown_percentage() == int(args.shutdown_percentage):
+                    print(f"Success, shutdown battery percentage: {pipower5.read_shutdown_percentage()}%")
     
     if args.input_voltage:
-        print(f"Input voltage: {spc.read_input_voltage()} mV")
+        print(f"Input voltage: {pipower5.read_input_voltage()} mV")
     if args.output_voltage:
-        print(f"Output voltage: {spc.read_output_voltage()} mV")
+        print(f"Output voltage: {pipower5.read_output_voltage()} mV")
     if args.output_current:
-        print(f"Output current: {spc.read_output_current()} mA")
+        print(f"Output current: {pipower5.read_output_current()} mA")
     if args.battery_voltage:
-        print(f"Battery voltage: {spc.read_battery_voltage()} mV")
+        print(f"Battery voltage: {pipower5.read_battery_voltage()} mV")
     if args.battery_current:
-        print(f"Battery current: {spc.read_battery_current()} mA")
+        print(f"Battery current: {pipower5.read_battery_current()} mA")
     if args.battery_percentage:
-        print(f"Battery percentage: {spc.read_battery_percentage()} %")
+        print(f"Battery percentage: {pipower5.read_battery_percentage()} %")
     if args.battery_source:
-        power_source = spc.read_power_source()
-        print(f"Power source: {power_source} ({'Battery' if power_source == spc.BATTERY else 'External'})")
+        power_source = pipower5.read_power_source()
+        print(f"Power source: {power_source} ({'Battery' if power_source == pipower5.BATTERY else 'External'})")
     if args.is_input_plugged_in:
-        print(f"Input plugged in: {spc.read_is_input_plugged_in()}")
+        print(f"Input plugged in: {pipower5.read_is_input_plugged_in()}")
     if args.is_charging:
-        print(f"Charging: {spc.read_is_charging()}")
+        print(f"Charging: {pipower5.read_is_charging()}")
     if args.default_on:
-        print(f"Default on: {'on' if spc.read_default_on() else 'off'}")
+        print(f"Default on: {'on' if pipower5.read_default_on() else 'off'}")
     if args.shutdown_request:
-        shutdown_request = spc.read_shutdown_request()
+        shutdown_request = pipower5.read_shutdown_request()
         shutdown_request_str = 'None'
-        if shutdown_request == spc.SHUTDOWN_REQUEST_NONE:
+        if shutdown_request == pipower5.SHUTDOWN_REQUEST_NONE:
             shutdown_request_str = 'None'
-        elif shutdown_request == spc.SHUTDOWN_REQUEST_LOW_BATTERY:
+        elif shutdown_request == pipower5.SHUTDOWN_REQUEST_LOW_BATTERY:
             shutdown_request_str = 'Low battery'
-        elif shutdown_request == spc.SHUTDOWN_REQUEST_BUTTON:
+        elif shutdown_request == pipower5.SHUTDOWN_REQUEST_BUTTON:
             shutdown_request_str = 'Button'
         else:
             shutdown_request_str = 'Unknown'
         print(f"Shutdown request: {shutdown_request} - {shutdown_request_str}")
     if args.charging_current:
-        print(f"Max charging current: {PiPower5.get_max_charge_current(spc)} mA")
+        print(f"Max charging current: {pipower5.get_max_charge_current()} mA")
     if args.all:
-        data_buffer = spc.read_all()
+        data_buffer = pipower5.read_all()
         print(f"Input voltage: {data_buffer['input_voltage']} mV")
         print(f"Output voltage: {data_buffer['output_voltage']} mV")
         print(f"Output current: {data_buffer['output_current']} mA")
         print(f"Battery voltage: {data_buffer['battery_voltage']} mV")
         print(f"Battery current: {data_buffer['battery_current']} mA")
         print(f"Battery percentage: {data_buffer['battery_percentage']} %")
-        print(f"Power source: {data_buffer['power_source']} - {'Battery' if data_buffer['power_source'] == spc.BATTERY else 'External'}")
+        print(f"Power source: {data_buffer['power_source']} - {'Battery' if data_buffer['power_source'] == pipower5.BATTERY else 'External'}")
         print(f"Input plugged in: {data_buffer['is_input_plugged_in']}")
         print(f"Charging: {data_buffer['is_charging']}")
         print('')
         print(f"Internal data:")
         shutdown_request_str = 'None'
-        if data_buffer['shutdown_request'] == spc.SHUTDOWN_REQUEST_NONE:
+        if data_buffer['shutdown_request'] == pipower5.SHUTDOWN_REQUEST_NONE:
             shutdown_request_str = 'None'
-        elif data_buffer['shutdown_request'] == spc.SHUTDOWN_REQUEST_LOW_BATTERY:
+        elif data_buffer['shutdown_request'] == pipower5.SHUTDOWN_REQUEST_LOW_BATTERY:
             shutdown_request_str = 'Low battery'
-        elif data_buffer['shutdown_request'] == spc.SHUTDOWN_REQUEST_BUTTON:
+        elif data_buffer['shutdown_request'] == pipower5.SHUTDOWN_REQUEST_BUTTON:
             shutdown_request_str = 'Button'
         else:
             shutdown_request_str = 'Unknown'
         print(f"Shutdown request: {data_buffer['shutdown_request']} - {shutdown_request_str}")
-        print(f"Max charging current: {PiPower5.get_max_charge_current(spc)} mA")
-        print(f"Default on: {'on' if spc.read_default_on() else 'off'}")
-        print(f"Shutdown percentage: {spc.read_shutdown_percentage()} %")
+        print(f"Max charging current: {PiPower5.get_max_charge_current(pipower5)} mA")
+        print(f"Default on: {'on' if pipower5.read_default_on() else 'off'}")
+        print(f"Shutdown percentage: {pipower5.read_shutdown_percentage()} %")
     if args.firmware:
-        print(f"Pipower5 firmware version: {spc.read_firmware_version()}")
+        print(f"Pipower5 firmware version: {pipower5.read_firmware_version()}")
 
     if args.enable_pwm_fan != '':
         if args.enable_pwm_fan == None:
@@ -253,12 +254,16 @@ def main():
         update_config_file(new_config, config_path)
 
     if args.command == "start":
-        from .pipower5 import PiPower5
         pipower5 = PiPower5(config_path=config_path)
         pipower5.set_debug_level(debug_level)
         pipower5.start()
 
-    if args.power_failure_simulation:
+    if args.power_failure_simulation != '':
+        test_time = 60 # seconds
+        pipower5.set_debug_level(debug_level)
+        pipower5.start()
+
+    if args.power_failure_simulation != '':
         test_time = 60 # seconds
         if args.power_failure_simulation != None:
             test_time = int(args.power_failure_simulation)
@@ -270,7 +275,7 @@ def main():
                 quit()
 
         print(f"Blackout simulation for {test_time} seconds")
-        report = PiPower5.power_failure_simulation(spc, test_time)
+        report = pipower5.power_failure_simulation(test_time)
         print(f'report:')
         print(f'  average battery voltage : {report["bat_voltage_avg"]:.3f} V')
         print(f'  average battery current : {report["bat_current_avg"]:.3f} A')
