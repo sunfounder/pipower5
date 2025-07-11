d60c86c78f9b5dd945bf03de4d66a6e4f47c2aeb
Lovmong <1967471283@qq.com>
2025-07-11 17:10:04 +0800

    Refactor read_all.py and read_individual.py for better readability

--- examples/read_all.py
+++ examples/read_all.py
@@ -9,20 +9,8 @@ def main():
     time.sleep(2)
 
     while True:
-        # Read the data before clearing the screen，to retain the last data when an error occurs.
         data_buffer = pipower5.read_all()
-        
-        print(f"Input voltage: {data_buffer['input_voltage']} mV")
-        print(f"Output voltage: {data_buffer['output_voltage']} mV")
-        print(f"Output current: {data_buffer['output_current']} mA")
-        print(f"Battery voltage: {data_buffer['battery_voltage']} mV")
-        print(f"Battery current: {data_buffer['battery_current']} mA")
-        print(f"Battery percentage: {data_buffer['battery_percentage']} %")
-        print(f"Power source: {data_buffer['power_source']} - {'Battery' if data_buffer['power_source'] == pipower5.BATTERY else 'External'}")
-        print(f"Input plugged in: {data_buffer['is_input_plugged_in']}")
-        print(f"Charging: {data_buffer['is_charging']}")
-        print('')
-        print(f"Internal data:")
+        #
         shutdown_request_str = 'None'
         if data_buffer['shutdown_request'] == pipower5.SHUTDOWN_REQUEST_NONE:
             shutdown_request_str = 'None'
@@ -32,12 +20,31 @@ def main():
             shutdown_request_str = 'Button'
         else:
             shutdown_request_str = 'Unknown'
-        print(f"Shutdown request: {data_buffer['shutdown_request']} - {shutdown_request_str}")
-        print(f"Max chargig current: {pipower5.i2c.read_byte_data(150)*100} mA")
-        print(f'Board id: {pipower5.read_board_id()}')
-        print(f"Default on: {pipower5.read_default_on()}")
-        print(f"Shutdown percentage: {pipower5.read_shutdown_percentage()} %")
+        #
+        print(f'''
+Input:
+    voltage: {data_buffer['input_voltage']} mV
+    current: {data_buffer['input_current']} mA
+    power: {data_buffer['input_voltage'] * data_buffer['input_current'] * 0.000001:.3f} W
+    plugged in: {data_buffer['is_input_plugged_in']}
+Output: 
+    voltage: {data_buffer['output_voltage']} mV
+    current: {data_buffer['output_current']} mA
+    power: {data_buffer['output_voltage'] * data_buffer['output_current'] * 0.000001:.3f} W
+Battery:
+    voltage: {data_buffer['battery_voltage']} mV
+    current: {data_buffer['battery_current']} mA
+    power: {data_buffer['battery_voltage'] * data_buffer['battery_current'] * 0.000001:.3f} W
+    percentage: {data_buffer['battery_percentage']} %
+    source: {data_buffer['power_source']} - {'Battery' if data_buffer['power_source'] == pipower5.BATTERY else 'External'}
+    charging: {data_buffer['is_charging']}
 
+Internal:
+    shutdown request: {data_buffer['shutdown_request']} - {shutdown_request_str}
+    max charging current: {pipower5.get_max_charge_current()} mA
+    default on: {'on' if pipower5.read_default_on() else 'off'}
+    shutdown percentage: {pipower5.read_shutdown_percentage()} %
+''')
         print('')
         print('')
         time.sleep(1)
